b72b381a7e19f4e4863f21faff60478df6fa358f
venutti <lvenutti@fi.uba.ar>
2023-01-23 11:25:16 -0300

    Elimina variables innecesarias

--- src/index.js
+++ src/index.js
@@ -4,18 +4,5 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
-const page = (
-  <div>
-    <h1>Fun facts about React</h1>
-    <ul>
-      <li>Was first released in 2013</li>
-      <li>Was originally created by Jordan Walke</li>
-      <li>Has well over 100K stars on Github</li>
-      <li>Is maintained by Facebook</li>
-      <li>Powers thousands of enterprise apps, including mobile apps</li>
-    </ul>
-  </div>
-);
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
